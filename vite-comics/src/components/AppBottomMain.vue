7be6e509a862c5e050a2716074ccf3f324b2d9b4
MirkoValle <mirko.valle20@gmail.com>
2024-06-11 20:22:35 +0200

    Added content on bottom main

--- vite-comics/src/components/AppBottomMain.vue
+++ vite-comics/src/components/AppBottomMain.vue
@@ -2,7 +2,33 @@
 export default{
     data() {
         return{
-            
+            blueLinks:[
+                {
+                    name: 'digital comics',
+                    url: '',
+                    src: 'buy-comics-digital-comics.png',
+                },
+                {
+                    name: 'dc merchandise',
+                    url: '',
+                    src: 'buy-comics-merchandise.png',
+                },
+                {
+                    name: 'subscription',
+                    url: '',
+                    src: 'buy-comics-subscriptions.png'
+                },
+                {
+                    name: 'comic shop locator',
+                    url: '',
+                    src: 'buy-comics-shop-locator.png'
+                },
+                {
+                    name: 'dc power visa',
+                    url: '',
+                    src: 'buy-dc-power-visa.svg'
+                },
+            ],
         }
     }
 }
@@ -11,7 +37,13 @@ export default{
 <template>
     <div class="container">
         <section>
-            <h1>prova</h1>
+            <ul>
+                <li v-for="(link, index) in blueLinks" :key="index">
+                    <a href="link.url"></a>
+                    <img :src="'../src/assets/img/' + link.src" alt="">
+                    <h2>{{ link.name }}</h2>
+                </li>
+            </ul>
         </section>
     </div>
 </template>
@@ -23,11 +55,34 @@ export default{
 
 .container{
     background-color: $blue-color;
-    padding: 3rem;
+    padding: 2rem;
     section{
         @include container;
-        h1{
-            color: white;
+        ul{
+            display: flex;
+            justify-content: space-between;
+
+            li{
+                display: flex;
+                align-items: center;
+                text-transform: uppercase;
+                padding: 1rem;
+                width: calc((100% / 5) );
+                cursor: pointer;
+
+                &:hover{
+                    background-color: rgba(231, 231, 231, 0.164);
+                }
+
+                img{
+                    width: 3rem;
+                }
+                h2{
+                    padding-left: 1rem;
+                    font-size: 1rem;
+                    color: white;
+                }
+            }
         }
     } 
 }

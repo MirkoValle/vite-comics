a060851d90efa781011326080946ac4d730f831c
MirkoValle <mirko.valle20@gmail.com>
2024-06-11 19:36:46 +0200

    Added comic Series as articles on main through another component

---
+++ vite-comics/src/components/ComicSeries.vue
@@ -0,0 +1,43 @@
+<script>
+export default{
+    data() {
+        return{
+            
+        }
+    },
+    props:{
+        singleComic:{
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
+
+<template>
+<article>
+    <img :src="singleComic.thumb" :alt="singleComic.series">
+    <h4>
+        {{ singleComic.series }}
+    </h4>
+</article>
+</template>
+
+<style lang="scss" scoped>
+
+    article{
+        width: calc((100% / 6) - 2rem);
+        padding: 1rem;
+
+        img{
+            width: 100%;
+            aspect-ratio: 1/1;
+            object-fit: cover;
+            object-position: top;
+        }
+        h4{
+            color: white;
+        }
+    }
+
+</style>
